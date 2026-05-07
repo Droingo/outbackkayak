156f8d8ddaad4b9503a9bdbf5da5de44fae10e90
Droingo <cmbmx57@gmail.com>
2026-05-07 19:58:36 +1000

    Initial commit

---
+++ src/main/java/net/droingo/outbackkayak/OutbackKayakClient.java
@@ -0,0 +1,10 @@
+package net.droingo.outbackkayak;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OutbackKayakClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/droingo/outbackkayak/OutbackKayakDataGenerator.java
@@ -0,0 +1,11 @@
+package net.droingo.outbackkayak;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OutbackKayakDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
